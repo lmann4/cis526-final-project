b2cc5931a55550d30aa7f314315dfa1a85dcc91b
brad-schoonover <badonmap@ksu.edu>
2017-04-20 12:37:24 -0500

    Added the schedule model, need to create migration, and populate(should be done as employee is added)

--- resource_management/apps/employees/models.py
+++ resource_management/apps/employees/models.py
@@ -13,3 +13,11 @@ class Employee(models.Model):
     salary = models.DecimalField(max_digits=8, decimal_places=2)
     start_date = models.DateField(default=date.today)
     end_date = models.DateField(blank=True, null=True)
+    
+class Schedule(models.Model):
+    employeeId = models.IntegerField()
+    date = models.DateField()
+    startTime = models.IntergerField()
+    endTime = models.IntergerField()
+    employee_sub = models.IntegerField()
+    up_for_sub = models.Booleanfield(default=False)
