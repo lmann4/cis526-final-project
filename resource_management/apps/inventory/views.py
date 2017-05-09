f2acab2ba79090ef614e61a9a000a33fb052ed56
brad-schoonover <badonmap@ksu.edu>
2017-05-08 23:27:11 -0500

    inventory is closer to being done

--- resource_management/apps/inventory/views.py
+++ resource_management/apps/inventory/views.py
@@ -9,14 +9,23 @@ from .forms import InventoryForm
 
 class InventoryCat(generic.ListView):
     model = Category
+    
+    def get_context_data(self, **kwargs):
+        context = super(InventoryCat, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        return context
 
 class InventoryList(generic.ListView):
     model = Inventory
     
+    def get_queryset(self):
+        queryset = Inventory.objects.filter(category=self.request.GET['category_id'])
+        return queryset
+        
     def get_context_data(self, **kwargs):
         context = super(InventoryList, self).get_context_data()
         context['categories'] = Category.objects.all()
-        print(self.request.GET['category_id'])
+        context['category'] = Category.objects.filter(id=self.request.GET['category_id']).first
         return context
 
 class createInventoryView(generic.CreateView):
